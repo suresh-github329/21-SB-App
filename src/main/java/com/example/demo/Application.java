7995134a20e000417ac18a4bc81124ef0898df27
Suresh <sureshkrishna206@gmail.com>
2021-05-25 10:50:52 +0530

    Main class Modified

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -9,5 +9,10 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	public void m1()
+	{
+		System.out.println("hello");
+	}
 
 }
