16723647a3aded4249fb5fc6a8ac2bde0cedd423
Suresh <sureshkrishna206@gmail.com>
2021-05-26 06:30:03 +0530

    add static variable

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+    static boolean flag = true;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
